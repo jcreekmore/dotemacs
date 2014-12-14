2ccd2368575e7eabe9ad428b4b77134e6789b639
Jonathan Creekmore <jonathan@thecreekmores.org>
2014-12-14 09:43:00 -0600

    Helm, org-trello, and org-jira added

---
+++ customize.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(haskell-font-lock-symbols t)
+ '(haskell-mode-hook (quote (turn-on-haskell-doc turn-on-haskell-indent)))
+ '(haskell-program-name "ghci")
+ '(ledger-use-iso-dates t)
+ '(org-agenda-files (quote ("~/Dropbox/Notes/trello.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -29,6 +29,7 @@ Return a list of installed packages or nil for every skipped package."
  'expand-region
  'gist
  'haskell-mode
+ 'helm
  'js2-mode
  'ledger-mode
  'linum-relative
@@ -39,6 +40,8 @@ Return a list of installed packages or nil for every skipped package."
  'magit-svn
  'markdown-mode
  'org
+ 'org-jira
+ 'org-trello
  'python
  'yasnippet
  )
@@ -47,7 +50,6 @@ Return a list of installed packages or nil for every skipped package."
 
 (tool-bar-mode -1)
 (setq backup-inhibited t)
-(ido-mode t)
 
 (setq custom-file (concat user-emacs-directory "customize.el"))
 (load custom-file)
@@ -57,4 +59,63 @@ Return a list of installed packages or nil for every skipped package."
 
 (color-theme-initialize)
 
+(eval-after-load "linum"
+  '(require 'linum-relative))
+
+(eval-after-load "org"
+  '(require 'org-trello))
+
 (global-set-key (kbd "<f12>") 'magit-status)
+
+(eval-after-load "align"
+  '(add-to-list 'align-rules-list
+                '(haskell-types
+                   (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+                   (modes quote (haskell-mode literate-haskell-mode)))))
+(eval-after-load "align"
+  '(add-to-list 'align-rules-list
+                '(haskell-assignment
+                  (regexp . "\\(\\s-+\\)=\\s-+")
+                  (modes quote (haskell-mode literate-haskell-mode)))))
+(eval-after-load "align"
+  '(add-to-list 'align-rules-list
+                '(haskell-arrows
+                  (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+                  (modes quote (haskell-mode literate-haskell-mode)))))
+(eval-after-load "align"
+  '(add-to-list 'align-rules-list
+                '(haskell-left-arrows
+                  (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
+                  (modes quote (haskell-mode literate-haskell-mode)))))
+
+
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
